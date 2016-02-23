0819a8851e7287d3ffa65e6e0b4842d0f3621810
Luke Diggins <l.pmd@hotmail.com>
2016-02-24 05:17:52 +1100

    Comments!

--- better-tw-friends.js
+++ better-tw-friends.js
@@ -23,13 +23,31 @@
 // @run-at          document-end
 // ==/UserScript==
 
+/**
+ * An array of Chat.Resource.Client-like (mostly) plain objects
+ * @type {Array}
+ */
 var friends = [];
+
+/**
+ * A map of player ids to unix timestamps
+ * @type {Object}
+ */
 var lastSent = {};
 
+/**
+ * Returns a list of keys for active events, eg Hearts. Practically guaranteed to have a length of 0 if no events are
+ * running and a length of 1 otherwise (2 or more = internal beta only).
+ * @returns {Array}
+ */
 function getActiveSesKeys() {
 	return Object.keys(Game.sesData);
 }
 
+/**
+ * Returns the number of friends you can currently send ses currency to. Friends list must be initiated first.
+ * @returns {Number}
+ */
 function getSesReadyCount() {
 	var yesterday = Date.now()/1000 - 3600*24;
 	var count = 0;
@@ -42,10 +60,19 @@ function getSesReadyCount() {
 	return count;
 }
 
+/**
+ * Returns the total number of friends you have. Friends list must be initiated first.
+ * @returns {Number}
+ */
 function getFriendCount() {
 	return friends.length;
 }
 
+/**
+ * Initiates the friend list and its twin, the last sent list. Do NOT run before establishing an event is ongoing.
+ * Returns a promise with the message from the server, if any, but consider it void if resolved successfully.
+ * @returns {Promise}
+ */
 function getFriendsList() {
 	return new Promise(function (resolve, reject) {
 		Ajax.remoteCallMode('friendsbar', 'search', {search_type: 'friends'}, function (data) {
@@ -68,6 +95,12 @@ function getFriendsList() {
 	});
 }
 
+/**
+ * Sends ses currency to a friend. Do NOT run before establishing an event is ongoing, even if you somehow obtain a
+ * friend id without initiating the friend list first (congratulations). Returns a promise with the response message.
+ * @param {Number} friendId
+ * @returns {Promise}
+ */
 function sendSesCurrency(friendId) {
 	return new Promise(function (resolve, reject) {
 		Ajax.remoteCall('friendsbar', 'event', {player_id: friendId, event: getActiveSesKeys()[0]}, function (data) {
